47fa85bf64d2b8e39c25ce7d73e485c70a6cd6ae
Nick Garza <thenickgarza14@gmail.com>
2022-01-18 22:10:09 -0600

    added the Employee class and started some tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee')
+
+test('creates a employee object', () => {
+    const employee = new Employee;
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('gets the name of the employee', () => {
+    const employee = new Employee;
+
+    expect(employee.getName()).toEqual(expect.any(String));;
+});
+
+test('gets the id of the employee', () => {
+    const employee = new Employee;
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets the email of the employee', () => {
+    const employee = new Employee;
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test('gets the role or returns the employee', () => {
+    const employee = new Employee;
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+})
